f7224891ea13006ca7553cbccdc654a70507fc4a
annoinspace <aneesahalmask@gmail.com>
2023-04-25 11:14:04 +0100

    stopwatch time display

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -23,32 +23,47 @@ export default function Time() {
     }
 
     if (isFirstFetch) {
-      fetchTime()
+      fetchTime() // set the first fetched time
     } else {
       const intervalId = setInterval(() => {
         fetchTime()
-      }, 30000)
+      }, 30000) // update every 30 seconds
 
       return () => clearInterval(intervalId)
     }
   }, [isFirstFetch])
 
+  const formatTimeDifference = (time1, time2) => {
+    const timeDiffInMillis = Math.abs(time1.getTime() - time2.getTime())
+    const hours = Math.floor(timeDiffInMillis / 3600000)
+    const minutes = Math.floor((timeDiffInMillis - hours * 3600000) / 60000)
+    const seconds = Math.floor((timeDiffInMillis - hours * 3600000 - minutes * 60000) / 1000)
+    return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds
+      .toString()
+      .padStart(2, "0")}`
+  }
+
   return (
     <div id="time-display-wrapper">
       <h1>Time</h1>
-      <div>Local Time {currentTime.toLocaleTimeString()}</div>
-      {isFirstFetch && allServerTimes.length > 0 && (
+
+      {isFirstFetch && allServerTimes.length < 0 && (
         <div className="current-difference" key={allServerTimes[0]}>
           <div>Server Time {allServerTimes[0]}</div>
-          <div>current difference</div>
         </div>
       )}
+      {/* only after the first fetch we render the following */}
       {!isFirstFetch && (
         <>
+          <div className="flex-row">
+            <div>Server Time</div>
+            <div>Current Difference</div>
+          </div>
           {allServerTimes.map((time) => (
             <div className="current-difference" key={time}>
-              <div>Server Time - {time}</div>
-              <div>current difference</div>
+              <div>{time}</div>
+
+              <div> {formatTimeDifference(currentTime, new Date(time * 1000))}</div>
             </div>
           ))}
         </>
